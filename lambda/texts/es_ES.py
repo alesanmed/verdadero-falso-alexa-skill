e0360a2849be6820ccc00e59f5ff905ba29718c9
Ale Sánchez <alesanchez@us.es>
2018-11-09 14:22:14 +0100

    Added locale files
    Added locale files for spanish (Spain) and spanish (Mexico)

---
+++ lambda/texts/es_ES.py
@@ -0,0 +1,4 @@
+HELP_TEXT = 'Dime que te haga una pregunta y empezaré a preguntarte cosas'
+EXIT_TEXT = 'Me lo he pasado muy bien, <prosody volume="x-loud" rate="fast">¡Hasta la próxima!</prosody>'
+EXCEPTION_TEXT = 'Parece que algo ha fallado, puedes decirme que vuelva a empezar'
+HELLO_TEXT = '¡Bienvenido a verdadero o falso! Dime que te haga una pregunta y te haré una de verdadero o falso, cuando la contestes te diré si tu respuesta es correcta o no y te preguntaré si quieres que te haga otra pregunta'
