25422cf4eb4acbcb5d8dc4d498f8c698672f180d
Ale Sánchez <alesanchez@us.es>
2018-11-13 10:57:58 +0100

    Isolated some code
    Isolated the code responsible of managing the process of retrieving and returning a new question

--- lambda/utils.py
+++ lambda/utils.py
@@ -1,14 +1,29 @@
 import importlib
+from db import db_functions
 
 STATES = {
   'QUESTION_ASKED': 0,
   'QUESTION_ANSWERED': 1
 }
 
+def new_question_process(handler_input, texts):
+  attr = handler_input.attributes_manager.session_attributes
+  attr['state'] = STATES['QUESTION_ASKED']
+
+  question_obj = db_functions.retrieve_random_question(handler_input)
+
+  attr['question_id'] == question_obj['_id']
+
+  speech_text = '{}, {}'.format(question_obj['question'], texts.TRUE_FALSE_TEXT)
+
+  return speech_text
+
+def check_correct_answer(question_obj, user_answer):
+  return bool(question_obj['correct_answer']) == bool(user_answer)
+
 def get_locale_texts(handler_input):
   return importlib.import_module('texts.{}'.format(
           __transform_locale(handler_input.request_envelope.request.locale)))
-
 
 def __transform_locale(locale):
   return locale.replace('-', '_')
