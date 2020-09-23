1bcdd5225d92977b9393ff21759592f86d31c2a5
Ale <ale@rebellionpay.com>
2020-09-23 22:17:12 +0200

    Changed log message to fstring

--- skill/handlers/LoggingRequestInterceptor.py
+++ skill/handlers/LoggingRequestInterceptor.py
@@ -7,10 +7,11 @@ class LoggingRequestInterceptor(AbstractRequestInterceptor):
     def process(self, handler_input):
         attr = handler_input.attributes_manager.session_attributes
         intent_received = 'Unknown'
+        user_state = attr.get('state', None)
 
         if handler_input.request_envelope.request.object_type == 'IntentRequest':
             intent_received = handler_input.request_envelope.request.intent.name
         else:
             intent_received = handler_input.request_envelope.request.object_type
 
-        logger.get_logger().info('User state: {}'.format(attr.get('state', None)))
+        logger.get_logger().info(f'Intent received: {intent_received}. User state: {user_state}')
