aa76da1cb5265dcb255391e86fb2e09c5dc1c67f
Ale <ale@rebellionpay.com>
2020-09-23 22:17:24 +0200

    Shortened some messages

--- skill/i18n/es_ES.py
+++ skill/i18n/es_ES.py
@@ -1,9 +1,9 @@
 SKILL_NAME = 'Verdadero o Falso'
 HELP_TEXT = 'Dime que te haga una pregunta y empezaré a preguntarte cosas'
 EXIT_TEXT = 'Me lo he pasado muy bien, ¡Hasta la próxima!'
-EXCEPTION_TEXT = 'Lo siento, pero no se cómo responder a eso. Prueba a repetir lo mismo o a decirme que vuelva a empezar.'
+EXCEPTION_TEXT = 'No se cómo responder a eso. Prueba a repetirlo o dime "vuelve a empezar".'
 EXCEPTION_REPROMPT_TEXT = 'Puedes repetir lo que has dicho o decirme "vuelve a empezar"'
-HELLO_TEXT = '¡Bienvenido a verdadero o falso! Dime que te haga una pregunta y te haré una de verdadero o falso, cuando la contestes te diré si tu respuesta es correcta o no y te preguntaré si quieres que te haga otra pregunta'
+HELLO_TEXT = '¡Bienvenido a verdadero o falso! Dime que te haga una pregunta y empezaré. Te diré si tu respuesta es correcta o no, y podrás pedirme otra'
 HELLO_REPROMPT_TEXT = 'Prueba a decirme, "hazme una pregunta"'
 TRUE_FALSE_TEXT = '¿Verdadero o Falso?'
 CORRECT_ANSWER_SPEECHCONS = ['ajá', 'bien bien', 'bingo', 'bravo', 'chapó', 'cierto', 'ding', 'equilicuá', 'eureka', 'excelente', 'mil euros', 'perfecto', 'sí señor', 'voilà']
